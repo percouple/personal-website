5aa492e7dcefc26f5f24faf15a4e498da01de170
Caleb Evans <calebsevan@gmail.com>
2024-04-23 13:35:36 -0500

    Orange styled underline

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,10 @@ const StyledIntroContainer = styled.div`
   margin-top: 10rem;
   margin-bottom: 15rem;
   background-color: #222222;
-  box-shadow: -10px -10px 0 10px green;  
-
+  max-width: 500px;
+  padding-right: 13px;
+  padding-bottom: 13px;
+  min-width: max-content;
 `;
 
 const StyledWelcomeParagraph = styled.div`
@@ -18,6 +20,16 @@ const StyledWelcomeParagraph = styled.div`
   max-width: 500px;
 `;
 
+const StyledCorner = styled.div`
+  position: absolute;
+  width: 384px;
+  height: 46px;
+  top: 420px;
+  background-color: #F17F29;
+  z-index: -1;
+  margin-left: 135px;
+`;
+
 const HeroName = styled.h1`
   color: #5c95ff;
   text-shadow: 3px 2px 0 #f17f29;
@@ -27,16 +39,16 @@ const HeroName = styled.h1`
 
 export default function Home() {
   return (
-    <div id="home" style={{backgroundColor: "#BDC4A7" }}>
-      <StyledIntroContainer> Styled intro container
-        <h3 style={{marginBottom: "0px", fontStyle: "italic"}}>Hello, I'm</h3>
+    <div style={{display: "flex", justifyContent: "center"}}>
+      <StyledCorner></StyledCorner>
+      <StyledIntroContainer id="home">
+        <h3 style={{ marginBottom: "0px", fontStyle: "italic" }}>Hello, I'm</h3>
         <HeroName>Caleb.</HeroName>
         <StyledWelcomeParagraph>
-          I'm a software developer eager to craft engaging experiences
-          tailored for entry-level learners. Currently, I'm building projects
-          geared towards expanding my proficiency across various coding
-          languages and platforms, enriching my versatility in
-          software development.
+          I'm a software developer eager to craft engaging experiences tailored
+          for entry-level learners. Currently, I'm building projects geared
+          towards expanding my proficiency across various coding languages and
+          platforms, enriching my versatility in software development.
         </StyledWelcomeParagraph>
       </StyledIntroContainer>
     </div>
