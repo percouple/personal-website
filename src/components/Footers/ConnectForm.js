acec63840a9674f9c2fd97a4dd1e6cd03e86796d
Caleb Evans <calebsevan@gmail.com>
2024-07-14 22:53:07 -0400

    Formspree implementation and styling

--- src/components/Footers/ConnectForm.js
+++ src/components/Footers/ConnectForm.js
@@ -1,47 +1,57 @@
 import React from "react";
 import styled from "styled-components";
-import { StyledButton, StyledInputField } from "../../styles/universalStyles";
+import { useForm, ValidationError } from "@formspree/react";
+import { StyledButton } from "../../styles/universalStyles";
 
 const FormContainer = styled.div`
   display: flex;
   flex-direction: column;
   margin-right: 2rem;
+  justify-content: space-between;
+  height: 12rem;
 `;
 
 const SubmitButton = styled(StyledButton)`
   min-height: 2rem;
   min-width: 10rem;
   margin-bottom: 0.5rem;
+  font-size: medium;
 `;
 
-const MessageField = styled(StyledInputField)`
+const EmailField = styled.input`
+  overflow-wrap: normal;
+  outline: none;
+  min-height: 1.25rem;
+`;
+
+const MessageField = styled.input`
   min-height: 5rem;
   min-width: 10rem;
   margin-bottom: 0.5rem;
-  /* max-width: 1rem; */
-  /* max-height: 1rem; */
+  overflow-wrap: normal;
+  outline: none;
 `;
 
-const SubjectField = styled(StyledInputField)`
-  min-height: 1.25rem;
-  min-width: 15rem;
-  max-width: 1rem;
-  max-height: 1rem;
-  margin-bottom: 0.5rem;
-`;
-
-export default function ConnectForm(props) {
+// Make sure to run npm install @formspree/react
+// For more help visit https://formspr.ee/react-help
+export default function ContactForm() {
+  const [state, handleSubmit] = useForm("manwnjpz");
+  if (state.succeeded) {
+    return (
+      <p>
+        Thanks for reaching out! I'll get back to you within 2 business days
+      </p>
+    );
+  }
   return (
-    <FormContainer>
-      <SubjectField
-        placeholder="Subject"
-        onChange={props.changeHandler}
-      ></SubjectField>
-      <MessageField
-        placeholder="Message"
-        onChange={props.changeHandler}
-      ></MessageField>
-      <SubmitButton onClick={props.submitHandler}>Open Mail</SubmitButton>
+    <FormContainer onSubmit={handleSubmit}>
+      <EmailField id="email" type="email" name="email" placeholder="email" />
+      <ValidationError prefix="Email" field="email" errors={state.errors} />
+      <MessageField id="message" name="message" placeholder="message" />
+      <ValidationError prefix="Message" field="message" errors={state.errors} />
+      <SubmitButton type="submit" disabled={state.submitting}>
+        Connect!
+      </SubmitButton>
     </FormContainer>
   );
 }
